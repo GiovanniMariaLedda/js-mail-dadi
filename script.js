6d4080c85f9dceb11a48c51d571c07452ed630f3
GiovanniMariaLedda <giovannimarialedda01@gmail.com>
2024-10-25 08:34:19 +0200

    add Mail compleate

--- script.js
+++ script.js
@@ -20,12 +20,20 @@ const emailList = ['filipet@gmail.com','gianma@icloud.com','franci@outlook.com',
 // Chiedere all'utente la sua email;
 const addUserEmail = prompt('inserisci la tua e-mail');
 // controllare che sia nella lista email di chi può accedere:
+const emailListPresent = emailList.includes(addUserEmail);
+console.log(emailListPresent);
 //  SE
+    if (emailListPresent == true){
 //    è nella lista, stampare messaggio OK;
+        console.log('presente');
+        window.alert("Sei presente nella lista");
+    }    
 //  ALTRIMENTI 
+    else { (emailListPresent == false)
 //    Stampare messaggio d'insuccesso.
-
-
+        console.log('Non presente');
+        window.alert("Non sei presente nella lista"); 
+    }
 
 
 
