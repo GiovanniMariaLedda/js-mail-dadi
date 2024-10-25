d292029b469615bab1bedddd406baff120536e71
GiovanniMariaLedda <giovannimarialedda01@gmail.com>
2024-10-25 09:04:29 +0200

    add Dadi compleate

--- scriptDadi.js
+++ scriptDadi.js
@@ -5,3 +5,37 @@ console.log('JS OK');
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+//! RAccolta Dati
+// Generare un numero random compreso fra 1 e 6;
+// Generarlo sia per il palyer che per il computer;
+// Vedere chi ha fatto il punteggio più alto;
+// paragonare i punteggi e decretare il vincitore;
+
+//! Procedimento 
+// Generatore numeri per palyer
+const playerNumber = Math.random(1) * 6 ;
+const playerIntNum = Math.ceil(playerNumber);
+console.log(playerIntNum);
+
+// Generatore numeri per computer 
+const computerNumber = Math.random(1) * 6 ;
+const computerIntNum = Math.ceil(computerNumber);
+console.log(computerIntNum);
+
+// Confronto numeri 
+if (playerIntNum > computerIntNum) {
+    console.log('Player Win');
+    window.alert('Player Win');
+
+} else if(computerIntNum > playerIntNum)  {
+    console.log('Computer Win');
+    window.alert('Computer Win');
+
+} else { (playerIntNum == computerIntNum)
+    console.log('pareggio');
+    window.alert('pareggio');
+}
+
+
+//! Output
